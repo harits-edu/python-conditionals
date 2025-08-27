c073afd22878e238c2204cdb425d7906c67eef60
Haritsyam Anshari <harits.edu@gmail.com>
2025-08-27 16:40:02 +0000

    Done

---
+++ compare2nd.py
@@ -0,0 +1,18 @@
+""" 
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y or x > y:
+    print("x is not equal to y")
+
+else:
+    print("x is equal to y")
+"""    
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
