8f27c170b15f9760282e8dffb2386c2ee57b57c9
Haritsyam Anshari <harits.edu@gmail.com>
2025-08-27 16:25:10 +0000

    Done

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
-
+# The first approach
+""" 
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
@@ -11,3 +12,32 @@ if x > y:
 if x == y:
     print("x is the same as y")
 
+"""
+
+# The second approach (much faster)
+""" 
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+elif x == y:
+    print("x is the same as y") 
+"""
+
+# The third approach, the perfect way, this approach is more efficient as seen from the flowchart
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+else:
+    print("x is the same as y") 
